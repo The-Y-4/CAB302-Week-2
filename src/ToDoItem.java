7dc6e7ef56985acb4e97dc23fae39ed020ee1d9b
RennyMatis2000 <rennysaurus@gmail.com>
2025-03-05 19:28:08 +1000

    Added ToDoItem class

---
+++ src/ToDoItem.java
@@ -0,0 +1,9 @@
+public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    public ToDoItem(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+}
